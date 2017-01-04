9aad64d6f836e0c9cf5b453e8ef3456151fc2def
William Reed <reedw@the-exeter.com>
2017-01-04 16:53:48 +0000

    The Javascript function now works and has been applied to two divs - still need to make it smarter

--- Script.js
+++ Script.js
@@ -1,3 +1,7 @@
 function showHtmlFramework(){
   document.getElementById("Html-Framework").style.visibility= "visible";
 }
+
+function showGitCommands(){
+  document.getElementById("Git-commands").style.visibility= "visible";
+}
